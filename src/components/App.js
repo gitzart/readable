96cff5d8402af8cc7d8f92e55f3c30b5aa0e1aa5
gitzart <windowxpxp@gmail.com>
2017-09-10 20:26:03 +0630

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Readable App
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+
+import './index.css'
+import registerServiceWorker from './registerServiceWorker'
+import reducer from './reducers'
+import App from './components/App'
+
+console.log(thunk);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  || compose
+
+
+const middlewares = [ thunk, logger ]
+
+const store = composeEnhancers(applyMiddleware(...middlewares))(createStore)(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+)
+
+registerServiceWorker()
